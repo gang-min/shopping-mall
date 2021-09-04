b73ce6e8be9227e87ab777cfb2939989fbcca40e
gang-min <rudals8920@naver.com>
2021-09-04 15:46:29 +0900

    PostMan과 axios를 활용해서 서버와 네트워크 통신

--- main.js
+++ main.js
@@ -1,48 +1,37 @@
 'use strict';
 const productList = document.querySelector('#product__list');
 
-const products = [
-  {
-    name: '농구공',
-    price: 100000,
-    seller: '조던',
-    imageUrl : '/images/products/basketball1.jpeg'
-  },
-  {
-    name: '축구공',
-    price: 50000,
-    seller: '메시',
-    imageUrl : '/images/products/soccerball1.jpg'
-  },
-  {
-    name: '키보드',
-    price: 10000,
-    seller: '갱민',
-    imageUrl : '/images/products/keyboard1.jpg'
-  },
-];
+const promiseObj = axios.get('https://316eeb27-2347-438e-aaf5-0e104e18aea5.mock.pstmn.io/products');
 
-let productsHtml = '';
-for(let i = 0; i < products.length; i++){
-let product = products[i];
-
-productsHtml +=       
-  `<li class="product__card">
-    <div class="product__img">
-      <img src="${product.imageUrl}" alt="${product.name}"/>
-    </div>
-    <div class="product__contents">
-      <span class="product__name">${product.name}</span>
-      <span class="product__price">${product.price}원</span>
-      <div class="product__seller">
-        <img class="seller__avatar" src="/images/icons/avatar.png" alt="아바타이미지"/>
-        <span class="seller__name">${product.seller}</span>
+promiseObj.then(function(response){
+  const products = response.data.products;
+  let productsHtml = '';
+  for(let i = 0; i < products.length; i++){
+  let product = products[i];
+  
+  productsHtml +=       
+    `<li class="product__card">
+      <div class="product__img">
+        <img src="${product.imageUrl}" alt="${product.name}"/>
       </div>
-    </div>
-  </li>`
-};
+      <div class="product__contents">
+        <span class="product__name">${product.name}</span>
+        <span class="product__price">${product.price}원</span>
+        <div class="product__seller">
+          <img class="seller__avatar" src="/images/icons/avatar.png" alt="아바타이미지"/>
+          <span class="seller__name">${product.seller}</span>
+        </div>
+      </div>
+    </li>`
+  };
+  
+  productList.innerHTML = productsHtml;
+  }).catch(function(error){
+  console.error(error);
+});
 
-productList.innerHTML = productsHtml;
+
+
 
 
 
